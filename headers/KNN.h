203585ca83d210858eee343a79790e38e8da3f83
Peter <lin.peter@outlook.fr>
2024-01-12 15:57:03 +0100

    2nd commit hoping it won't crash

---
+++ headers/KNN.h
@@ -0,0 +1,22 @@
+//
+// Created by Istiak on 12/01/2024.
+//
+
+#ifndef CPP_EXAM_KNN_H
+#define CPP_EXAM_KNN_H
+
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class KNN {
+private:
+    int k;
+public:
+    KNN();
+    KNN(int);
+
+};
+
+#endif //CPP_EXAM_KNN_H
